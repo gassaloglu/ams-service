f935fc943d49f49839fb0a804947f08cf552524c
Yusuf Gassaloğlu <yusufgassaloglu00@gmail.com>
2025-01-17 14:17:59 +0300

    implement logger

--- config/config.go
+++ config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	ServerPort string `yaml:"server_port"`
+	Database   struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+}
+
+func LoadConfig() (*Config, error) {
+	data, err := ioutil.ReadFile("config/config.yaml")
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
--- middlewares/error_handler.go
+++ middlewares/error_handler.go
@@ -1,1 +1,21 @@
 package middlewares
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ErrorHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		if len(c.Errors) > 0 {
+			for _, e := range c.Errors {
+				log.Printf("Error: %v", e.Err)
+			}
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		}
+	}
+}
