8562c35635c4bc0d53859478bbf119f615f94ad1
Berkay Dinç <berkayydinc@gmail.com>
2025-04-01 20:57:06 +0300

    fix: Rename secondary ports as repositories, remove datagen controller

--- internal/adapters/primary/datagen/controller.go
+++
@@ -1,9 +0,0 @@
-package datagen
-
-import "ams-service/internal/core/services"
-
-type DatagenController struct {
-	bankService     services.BankService
-	employeeService services.EmployeeServiceImpl
-	flightService   services.FlightService
-}
